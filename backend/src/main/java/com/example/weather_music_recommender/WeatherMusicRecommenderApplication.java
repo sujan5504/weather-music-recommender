4513a90818ddd16af138a7dcf7c24c2062ae3b0c
sujan bhattarai <39189458+sujan5504@users.noreply.github.com>
2026-03-29 00:32:42 -0400

    Add main application class for Spring Boot

---
+++ backend/src/main/java/com/example/weather_music_recommender/WeatherMusicRecommenderApplication.java
@@ -0,0 +1,11 @@
+package com.example.weather_music_recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMusicRecommenderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherMusicRecommenderApplication.class, args);
+    }
+}
